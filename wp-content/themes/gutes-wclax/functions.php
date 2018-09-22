b8b1a7fd93813a4af494270346648fd87935f1d3
Roy Sivan <roy@roysivan.com>
2018-09-22 08:23:28 -0700

    adding cpt's to function.php

---
+++ wp-content/themes/gutes-wclax/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+add_action( 'init', 'my_custom_cpts', 10 );
+function my_custom_cpts() {
+	if ( ! defined( 'GUTENBERG_OBJECT_PLUGIN_CPTS' ) ) {
+		define( 'GUTENBERG_OBJECT_PLUGIN_CPTS', 'post,page' );
+	}
+}
+
--- wp-content/themes/gutes-wclax/index.php
+++ wp-content/themes/gutes-wclax/index.php
@@ -1,12 +1,4 @@
 <?php
-
-add_action( 'init', 'my_custom_cpts', 10 );
-function my_custom_cpts() {
-	if ( ! defined( 'GUTENBERG_OBJECT_PLUGIN_CPTS' ) ) {
-		define( 'GUTENBERG_OBJECT_PLUGIN_CPTS', 'post,page' );
-	}
-}
-
 $page = get_page_by_path( 'welcome' );
 if ( $page ) {
 	wp_safe_redirect( get_rest_url() . 'wp/v2/pages/' . $page->ID );
