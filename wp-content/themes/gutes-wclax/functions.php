de8a7277a9634ac9937bdad72bb0bb7fc9b20933
Roy Sivan <roy@roysivan.com>
2018-09-22 12:56:20 -0700

    Theme Support

--- wp-content/themes/gutes-wclax/functions.php
+++ wp-content/themes/gutes-wclax/functions.php
@@ -1,9 +1,8 @@
 <?php
+add_theme_support( 'post-thumbnails' );
 
 add_action( 'init', 'my_custom_cpts', 10 );
 function my_custom_cpts() {
-
-	add_theme_support( 'post-thumbnail' );
 
 	if ( ! defined( 'GUTENBERG_OBJECT_PLUGIN_CPTS' ) ) {
 		define( 'GUTENBERG_OBJECT_PLUGIN_CPTS', 'post,page' );
