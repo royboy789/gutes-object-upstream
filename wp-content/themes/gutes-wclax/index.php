6e836bf9a0e11b67a557ec3a73fe8e3e98002028
Roy Sivan <roy@roysivan.com>
2019-02-15 20:55:58 -0700

    fixing rest api redirect

--- wp-content/themes/gutes-wclax/index.php
+++ wp-content/themes/gutes-wclax/index.php
@@ -2,7 +2,7 @@
 $page = get_page_by_path( 'welcome' );
 if ( $page ) {
 	wp_safe_redirect( get_the_permalink( $page->ID ) );
-	wp_safe_redirect( get_rest_url() . '/wp/v2/page/' . $page->ID );
+	wp_safe_redirect( get_rest_url() . 'wp/v2/pages/' . $page->ID );
 } else {
-	wp_safe_redirect( get_rest_url() . '/wp/v2/posts' );
+	wp_safe_redirect( get_rest_url() . 'wp/v2/posts' );
 }
