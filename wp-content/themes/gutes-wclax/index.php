aae38426fc77c98e92637656b15ca2085a93d418
Roy Sivan <roy@roysivan.com>
2019-02-15 20:54:09 -0700

    Update to Pantheon Banner & home redirect

--- wp-content/themes/gutes-wclax/index.php
+++ wp-content/themes/gutes-wclax/index.php
@@ -1,2 +1,8 @@
 <?php
-wp_safe_redirect( get_rest_url() . 'wp/v2/posts' );
+$page = get_page_by_path( 'welcome' );
+if ( $page ) {
+	wp_safe_redirect( get_the_permalink( $page->ID ) );
+	wp_safe_redirect( get_rest_url() . '/wp/v2/page/' . $page->ID );
+} else {
+	wp_safe_redirect( get_rest_url() . '/wp/v2/posts' );
+}
