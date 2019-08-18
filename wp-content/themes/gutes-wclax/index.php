223952400cffcea9169c38458f061b5e9dfe1b73
Roy Sivan <roy@roysivan.com>
2019-08-18 13:17:58 -0700

    changing theme name and directory

--- wp-content/themes/gutes-wclax/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$page = get_page_by_path( 'welcome' );
-if ( $page ) {
-	wp_safe_redirect( get_the_permalink( $page->ID ) );
-	wp_safe_redirect( get_rest_url() . 'wp/v2/pages/' . $page->ID );
-} else {
-	wp_safe_redirect( get_rest_url() . 'wp/v2/posts' );
-}
